f7577ce6a3ba9a6efb03d3b785a210449907346d
Zeptosec <86299007+Zeptosec@users.noreply.github.com>
2021-06-22 21:31:34 +0300

    Mandelbrot set on wpf
    Ability to move around and scroll in and out

---
+++ mandelbrotset/MandelbrotSet/Calculations.cs
@@ -0,0 +1,124 @@
+﻿using Cudafy;
+using Cudafy.Host;
+using Cudafy.Translator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace MandelbrotSet
+{
+    public static class Calculations
+    {
+        private const int maxIter = 1000;
+        public const int threadsPerBlock = 256;
+        public const int blocksPerGrid = 32;
+        private static GPGPU gpu;
+
+        public static void Setup()
+        {
+            CudafyModes.Target = eGPUType.OpenCL;
+            CudafyTranslator.Language = eLanguage.OpenCL;
+            CudafyModule km = CudafyTranslator.Cudafy();
+
+            gpu = CudafyHost.GetDevice(CudafyModes.Target, CudafyModes.DeviceId);
+            gpu.LoadModule(km);
+        }
+
+        public static void SetImageCPU(Bitmap image, double zoom, int offX, int offY)
+        {
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    int it = GetIteration(
+                        TransformNumber(i + offX, 0, image.Width, -zoom, zoom),
+                        TransformNumber(j + offY, 0, image.Height, -zoom, zoom));
+                    byte rounded = (byte)((float)it / maxIter * 255);
+                    image.SetPixel(i, j, Color.FromArgb(rounded, rounded, rounded));
+                }
+            }
+        }
+
+        public static void SetImage(Bitmap image, double zoom, int offX, int offY)
+        {
+            Rectangle area = new Rectangle(0, 0, image.Width, image.Height);
+            BitmapData bitmapData = image.LockBits(area, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            int stride = bitmapData.Stride;
+            IntPtr ptr = bitmapData.Scan0;
+            int numBytes = bitmapData.Stride * image.Height;
+            byte[] rgbValues = new byte[numBytes];
+
+            try
+            {
+                byte[] dev_rgb = gpu.Allocate(rgbValues);
+                gpu.Launch(blocksPerGrid, threadsPerBlock).GetValues(dev_rgb, zoom, stride, image.Height, offX, offY);
+                gpu.CopyFromDevice(dev_rgb, rgbValues);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                gpu.FreeAll();
+            }
+
+            Marshal.Copy(rgbValues, 0, ptr, bitmapData.Stride * image.Height);
+            image.UnlockBits(bitmapData);
+        }
+
+        [Cudafy]
+        static int GetIteration(double x0, double y0)
+        {
+            double x = 0, y = 0;
+            int iter = 0;
+            while (x * x + y * y <= 4 && iter < maxIter)
+            {
+                double xtemp = x * x - y * y + x0;
+                y = 2 * x * y + y0;
+                x = xtemp;
+                iter++;
+            }
+            return iter;
+        }
+
+        [Cudafy]
+        public static double TransformNumber(double x, int startX, int endX, double toStartX, double toEndX)
+        {
+            int d = endX - startX;
+            return x / d * (toEndX - toStartX) + toStartX;
+        }
+
+        [Cudafy]
+        public static void GetValues(GThread thread, byte[] rgbValues, double zoom, int stride, int height, int offX, int offY)
+        {
+            int threadID = thread.threadIdx.x + thread.blockIdx.x * thread.blockDim.x;
+            int numThreads = thread.blockDim.x * thread.gridDim.x;
+            if (threadID < rgbValues.Length)
+            {
+                for (int i = threadID; i < stride / 3 * height; i += numThreads)
+                {
+                    int x = i % (stride / 3);
+                    int y = i / (stride / 3);
+
+                    int iters = GetIteration(
+                        TransformNumber(x + offX, 0, stride / 3, -zoom, zoom),
+                        TransformNumber(y + offY, 0, height, -zoom, zoom));
+                    byte rounded = (byte)((float)iters / maxIter * 255);
+
+                    int rPos = y * stride + x * 3;
+
+                    rgbValues[rPos] = rounded;
+                    rgbValues[rPos + 1] = rounded;
+                    rgbValues[rPos + 2] = rounded;
+                }
+            }
+        }
+    }
+}
---
+++ mandelbrotset/MandelbrotSet/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+using System.IO;
+using System;
+using System.Diagnostics;
+
+namespace MandelbrotSet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double zoom = .5;
+        private double dx = 300, dy = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Calculations.Setup();
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        Bitmap bm;
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            bm = new Bitmap((int)e.NewSize.Width, (int)e.NewSize.Height);
+            //Stopwatch sw = new Stopwatch();
+            //sw.Start();
+            Calculations.SetImage(bm, zoom, (int)dx, (int)dy);
+            //sw.Stop();
+            //Console.WriteLine($"GPU time: {sw.ElapsedMilliseconds} ms");
+            //sw.Restart();
+            //Calculations.SetImageCPU(bm, zoom, (int)dx, (int)dy);
+            //sw.Stop();
+            //Console.WriteLine($"CPU time: {sw.ElapsedMilliseconds} ms");
+
+            ImageViewer.Source = BitmapToImageSource(bm);
+        }
+
+        System.Windows.Point pressPos;
+        private void ImageViewer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                pressPos = e.GetPosition(this);
+            }
+        }
+
+        System.Windows.Point newPos;
+        private void ImageViewer_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                newPos = e.GetPosition(this);
+                double tmpdx = pressPos.X - newPos.X;
+                double tmpdy = pressPos.Y - newPos.Y;
+                Calculations.SetImage(bm, zoom, (int)(dx + tmpdx), (int)(dy + tmpdy));
+                ImageViewer.Source = BitmapToImageSource(bm);
+            }
+        }
+
+        private void ImageViewer_IsMouseDirectlyOverChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue == false) // mouse left window
+            {
+                dx += pressPos.X - newPos.X;
+                dy += pressPos.Y - newPos.Y;
+            }
+        }
+
+        private void ImageViewer_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if(e.Delta < 0) // scroll back to zoom out
+            {
+                zoom *= 1.1;
+                dx /= 1.1;
+                dy /= 1.1;
+            }
+            else // scroll in to zoom in
+            {
+                zoom *= .9;
+                dx /= .9;
+                dy /= .9;
+            }
+            Console.WriteLine($"zoom: {zoom}; dx: {dx}; dy: {dy}");
+            Calculations.SetImage(bm, zoom, (int)dx, (int)dy);
+            ImageViewer.Source = BitmapToImageSource(bm);
+        }
+
+        private void ImageViewer_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Released)
+            {
+                System.Windows.Point newPos = e.GetPosition(this);
+                dx += pressPos.X - newPos.X;
+                dy += pressPos.Y - newPos.Y;
+            }
+        }
+    }
+}
